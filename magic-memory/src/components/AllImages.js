db238370f2c05b406bdf8cc321067319b8e743be
SimonS2019 <shentiexiong@gmail.com>
2024-01-19 12:30:11 +1300

    Refactor AllImages component to display images in a grid

--- magic-memory/src/components/AllImages.js
+++ magic-memory/src/components/AllImages.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 export default function AllImages({ cards }) {
   return (
-    <div className="all-images">
+    <div style={{ display: "flex", flexWrap: "wrap" }}>
       {cards.map((card, index) => (
-        <img key={index} src={card.src} alt={`card-${index}`} />
+        <div key={index} style={{ flex: "0 0 auto", margin: "5px" }}>
+          <img
+            src={card.src}
+            alt={`card-${index}`}
+            style={{ width: "200px", height: "200px", objectFit: "cover" }}
+          />
+        </div>
       ))}
     </div>
-  )
+  );
 }
