c4393448561c2bc6edc9fb34fec1ab2591163f61
SimonS2019 <shentiexiong@gmail.com>
2024-01-18 14:10:07 +1300

    65 Refactor card flipping logic and styles

--- magic-memory/src/components/SingleCard.js
+++ magic-memory/src/components/SingleCard.js
@@ -1,18 +1,19 @@
-import React from 'react'
 import './SingleCard.css'
 
-export default function SingleCard({ card, handleChoice }) {
+export default function SingleCard({ card, handleChoice, flipped }) {
 
-    const handleClick = () => {
-      handleChoice(card)
-    }
-  
-    return (
-      <div className="card">
-        <div>
-          <img className="front" src={card.src} alt="card front" />
-          <img className="back" src="/img/cover.png" onClick={handleClick} alt="cover" />
-        </div>
-      </div>
-    )
+  const isFlipped = flipped || card.matched
+
+  const handleClick = () => {
+    handleChoice(card)
   }
+
+  return (
+    <div className="card">
+      <div className={isFlipped ? "flipped" : ""}>
+        <img className="front" src={card.src} alt="card front" />
+        <img className="back" src="/img/cover.png" onClick={handleClick} alt="cover" />
+      </div>
+    </div>
+  )
+}
